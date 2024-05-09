5ba1f1fe569a8f3ce2343cd4b4694285c33f2180
noujoud <ayaninoujoud1@gmail.com>
2024-05-09 20:14:49 +0100

    added fonction component + route file

--- src/app/shared/model/employe.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Employe } from './employe.model';
-
-describe('Employe', () => {
-  it('should create an instance', () => {
-    expect(new Employe()).toBeTruthy();
-  });
-});
---
+++ src/app/views/OurComponents/fonction/fonction-list/fonction-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fonction-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './fonction-list.component.html',
+  styleUrl: './fonction-list.component.scss'
+})
+export class FonctionListComponent {
+
+}
