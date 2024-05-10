498a3346330319369775dcbf158374b367422fc9
AablaEssadaq <aabla2003@gmail.com>
2024-05-10 16:39:22 +0100

    updated the profile component

--- src/app/views/OurComponents/profile/profile-view/profile-view.component.ts
+++ src/app/views/OurComponents/profile/profile-view/profile-view.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-profile-view',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './profile-view.component.html',
   styleUrl: './profile-view.component.scss'
 })
