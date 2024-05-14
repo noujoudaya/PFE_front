ff99ccfea1a64862b105ffc0d1b31cdf3df52ae2
AablaEssadaq <aabla2003@gmail.com>
2024-05-14 14:11:41 +0100

    get authenticated employee infos + display them in the profile component

--- src/app/services/services/employe.service.ts
+++ src/app/services/services/employe.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Employe} from "../models/employe.model";
 import {Observable} from "rxjs";
+import value from "*.json";
 
 @Injectable({
   providedIn: 'root'
@@ -10,30 +11,39 @@ export class EmployeService {
 
   private _employe: Employe = new Employe;
   private _employes: Employe[] = [];
+
+  // @ts-ignore
+  public _authenticatedEmploye: Employe;
+
   private url: string = "http://localhost:8088/api/v1/employes/";
 
-  constructor(private http:HttpClient) { }
-
-  public save() : Observable<number>{
-    return this.http.post<number>(this.url+'save',this.employe);
+  constructor(private http: HttpClient) {
   }
 
-  public update(): Observable<number>{
-    return this.http.post<number>(this.url+'update',this.employe)
+  public save(): Observable<number> {
+    return this.http.post<number>(this.url + 'save', this.employe);
   }
-  public findAll() :Observable<Array<Employe>>{
+
+  public update(): Observable<number> {
+    return this.http.post<number>(this.url + 'update', this.employe)
+  }
+
+  public findAll(): Observable<Array<Employe>> {
     return this.http.get <Array<Employe>>(this.url);
   }
+
   public searchEmployes(term: string): Observable<Array<Employe>> {
-    return this.http.get<Array<Employe>>(this.url + 'search', { params: { search: term } });
+    return this.http.get<Array<Employe>>(this.url + 'search', {params: {search: term}});
   }
 
   public getEmployeeCount(): Observable<number> {
     return this.http.get<number>(this.url + 'count');
   }
-  public deleteByCin(cin:string): Observable<number>{
-    return this.http.delete<number>(this.url+'cin/'+cin);
+
+  public deleteByCin(cin: string): Observable<number> {
+    return this.http.delete<number>(this.url + 'cin/' + cin);
   }
+
   get employe(): Employe {
     return this._employe;
   }
@@ -48,5 +58,14 @@ export class EmployeService {
 
   set employes(value: Employe[]) {
     this._employes = value;
+  }
+
+
+  get authenticatedEmploye(): Employe {
+    return this._authenticatedEmploye;
+  }
+
+  set authenticatedEmploye(value: Employe) {
+    this._authenticatedEmploye = value;
   }
 }
--- src/app/views/OurComponents/profile/profile-view/profile-view.component.ts
+++ src/app/views/OurComponents/profile/profile-view/profile-view.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {NgIf} from "@angular/common";
+import {EmployeService} from "../../../../services/services/employe.service";
+import {Employe} from "../../../../services/models/employe.model";
 
 @Component({
   selector: 'app-profile-view',
@@ -12,6 +14,19 @@ import {NgIf} from "@angular/common";
   templateUrl: './profile-view.component.html',
   styleUrl: './profile-view.component.scss'
 })
-export class ProfileViewComponent {
+export class ProfileViewComponent implements OnInit{
 
+  // @ts-ignore
+  authenticatedEmploye: Employe;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const storedEmployee = localStorage.getItem('authenticatedEmploye');
+    if (storedEmployee) {
+      this.authenticatedEmploye = JSON.parse(storedEmployee);
+    }
+  }
+
+  protected readonly Array = Array;
 }
